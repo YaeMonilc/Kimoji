9650eea5b919c2626ad7e81c43b056f4b83b3d30
YaeMonilc <Monilc@outlook.com>
2024-07-18 17:34:05 +0800

    增加对 Picacg 的Api支持

--- app/src/main/java/moe/wisteria/android/network/NetworkHelper.kt
+++ app/src/main/java/moe/wisteria/android/network/NetworkHelper.kt
@@ -2,15 +2,33 @@ package moe.wisteria.android.network
 
 import moe.wisteria.android.network.api.ChannelApi
 import moe.wisteria.android.network.api.PicacomicApi
+import moe.wisteria.android.util.HmacUtils
 import okhttp3.Dns
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.net.InetAddress
 import java.net.ProxySelector
+import java.util.UUID
 
 const val CHANNEL_SERVER_URL = "http://68.183.234.72"
 const val PICACOMIC_SERVER_URL = "https://picaapi.picacomic.com"
+
+private object PicacomicConstValue {
+    const val API_KEY = "C69BAF41DA5ABD1FFEDC6D2FEA56B"
+    const val APP_CHANNEL = "1"
+    const val APP_VERSION = "2.2.1.3.3.4"
+    const val APP_UUID = "defaultUuid"
+    const val IMAGE_QUALITY = "original"
+    const val APP_PLATFORM = "android"
+    const val APP_BUILD_VERSION = "45"
+    const val ACCEPT = "application/vnd.picacomic.com.v1+json"
+    const val USER_AGENT = "okhttp/4.12.0"
+    const val CONTENT_TYPE = "application/json; charset=UTF-8"
+    const val HOST = "picaapi.picacomic.com"
+
+    const val HMAC_KEY = "~d}\$Q7\$eIni=V)9\\RK/P.RM4;9[7|@/CA}b~OW!3?EV`:<>M7pddUBL5n|0/*Cn"
+}
 
 var defaultOkHttpClient: OkHttpClient = getOkhttpClient()
     private set
@@ -35,6 +53,7 @@ private fun getBaseRetrofit(
     addConverterFactory(GsonConverterFactory.create())
 }.build()
 
+@OptIn(ExperimentalStdlibApi::class)
 private fun getOkhttpClient(
     dnsUrl: String? = null,
 ) = OkHttpClient.Builder().apply {
@@ -51,7 +70,36 @@ private fun getOkhttpClient(
     addInterceptor {
         it.proceed(
             it.request().newBuilder().apply {
+                it.request().let { request ->
+                    val url = request.url.toString()
 
+                    if (url.startsWith(PICACOMIC_SERVER_URL)) {
+                        PicacomicConstValue.apply {
+                            val path = url.replace(PICACOMIC_SERVER_URL, "")
+                            val nonce = UUID.randomUUID().toString().replace("-", "")
+                            val time = (System.currentTimeMillis() / 1000).toString()
+                            val signature = HmacUtils.hmacSHA256(
+                                key = HMAC_KEY,
+                                data = "$path$time$nonce${request.method}$${ API_KEY }".lowercase()
+                            ).toHexString()
+
+                            addHeader("api-key", API_KEY)
+                            addHeader("app-channel", APP_CHANNEL)
+                            addHeader("time", time)
+                            addHeader("nonce", nonce)
+                            addHeader("signature", signature)
+                            addHeader("app-version", APP_VERSION)
+                            addHeader("app-uuid", APP_UUID)
+                            addHeader("image-quality", IMAGE_QUALITY)
+                            addHeader("app-platform", APP_PLATFORM)
+                            addHeader("app-build-version", APP_BUILD_VERSION)
+                            addHeader("accept", ACCEPT)
+                            addHeader("User-Agent", USER_AGENT)
+                            addHeader("Content-Type", CONTENT_TYPE)
+                            addHeader("Host", HOST)
+                        }
+                        }
+                }
             }.build()
         )
     }
--- app/src/main/java/moe/wisteria/android/network/api/PicacomicApi.kt
+++ app/src/main/java/moe/wisteria/android/network/api/PicacomicApi.kt
@@ -1,4 +1,15 @@
 package moe.wisteria.android.network.api
 
+import moe.wisteria.android.network.entity.body.SignInBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface PicacomicApi {
+    @POST("/auth/sign-in")
+    fun signIn(
+        @Body
+        body: SignInBody
+    ): Call<ResponseBody>
 }
