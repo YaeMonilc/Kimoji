fbda513e693e837fbfabb9ba1ad127e286114010
YaeMonilc <Monilc@outlook.com>
2024-07-19 16:38:45 +0800

    改进

--- app/src/main/java/moe/wisteria/android/util/LocalizationUtils.kt
+++ app/src/main/java/moe/wisteria/android/util/LocalizationUtils.kt
@@ -5,3 +5,9 @@ import moe.wisteria.android.R
 val localizationMapping: Map<String, Int> = mapOf(
     "invalid email or password" to R.string.network_invalid_email_or_password
 )
+
+fun getLocalization(
+    key: String
+): Int {
+    return localizationMapping[key] ?: R.string.network_unknown_response
+}
