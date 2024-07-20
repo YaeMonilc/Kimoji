c1cc2b0ee129bd603373b36ee6883fee085a3e85
YaeMonilc <Monilc@outlook.com>
2024-07-20 17:40:33 +0800

    移除未使用的引入

--- app/src/main/java/moe/wisteria/android/kimoji/network/api/ChannelApi.kt
+++ app/src/main/java/moe/wisteria/android/kimoji/network/api/ChannelApi.kt
@@ -1,8 +1,6 @@
 package moe.wisteria.android.kimoji.network.api
 
 import moe.wisteria.android.kimoji.network.entity.response.ChannelResponse
-import okhttp3.ResponseBody
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface ChannelApi {
