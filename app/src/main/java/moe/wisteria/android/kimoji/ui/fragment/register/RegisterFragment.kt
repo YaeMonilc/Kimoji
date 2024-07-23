708309f5bafbb2e8d3ef72f670de5066d9b23b15
YaeMonilc <Monilc@outlook.com>
2024-07-23 15:13:40 +0800

    完善 注册 页面UI

--- app/src/main/java/moe/wisteria/android/kimoji/ui/fragment/register/RegisterFragment.kt
+++ app/src/main/java/moe/wisteria/android/kimoji/ui/fragment/register/RegisterFragment.kt
@@ -1,16 +1,19 @@
 package moe.wisteria.android.kimoji.ui.fragment.register
 
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.datepicker.MaterialDatePicker
 import moe.wisteria.android.kimoji.R
 import moe.wisteria.android.kimoji.databinding.FragmentRegisterBinding
 import moe.wisteria.android.kimoji.ui.view.BaseFragment
 import moe.wisteria.android.kimoji.util.TextInputLayoutControllerList
+import java.util.Date
 
 class RegisterFragment : BaseFragment(
     toolBarOption = ToolBarOption(
@@ -22,6 +25,10 @@ class RegisterFragment : BaseFragment(
     private val viewModel: RegisterModel by viewModels()
 
     private lateinit var textInputLayoutControllerList: TextInputLayoutControllerList
+
+    companion object {
+        const val DATE_FORMAT = "yyyy-MM-dd"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +51,38 @@ class RegisterFragment : BaseFragment(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        textInputLayoutControllerList = TextInputLayoutControllerList(
+            context = requireContext()
+        ).apply {
+            addTextInputLayout(binding.fragmentRegisterEmail)
+            addTextInputLayout(binding.fragmentRegisterName)
+            addTextInputLayout(binding.fragmentRegisterPassword)
+            addTextInputLayout(binding.fragmentRegisterBirthday)
+            addTextInputLayout(binding.fragmentRegisterQuestion)
+            addTextInputLayout(binding.fragmentRegisterAnswer)
+        }
+
+        viewModel.let { viewModel ->
+            binding.fragmentRegisterBirthday.setEndIconOnClickListener {
+                MaterialDatePicker.Builder.datePicker().build().apply {
+                    addOnPositiveButtonClickListener {
+                        viewModel.birthday.value = DateFormat.format(DATE_FORMAT, Date(it)).toString()
+                    }
+                }.show(childFragmentManager, null)
+            }
+
+            binding.fragmentRegisterGender.addOnButtonCheckedListener { _, checkedId, _ ->
+                viewModel.gender.postValue(
+                    when (checkedId) {
+                        R.id.fragment_register_gender_man -> RegisterModel.Gender.MAN
+                        R.id.fragment_register_gender_woman -> RegisterModel.Gender.WOMAN
+                        R.id.fragment_register_gender_other -> RegisterModel.Gender.OTHER
+                        else -> RegisterModel.Gender.MAN
+                    }.realValue
+                )
+            }
+        }
     }
 
     override fun onStart() {
--- app/src/main/java/moe/wisteria/android/kimoji/ui/fragment/register/RegisterModel.kt
+++ app/src/main/java/moe/wisteria/android/kimoji/ui/fragment/register/RegisterModel.kt
@@ -6,9 +6,22 @@ import androidx.lifecycle.ViewModel
 import moe.wisteria.android.kimoji.entity.IndicatorState
 
 class RegisterModel : ViewModel() {
+    enum class Gender(
+        val realValue: String
+    ) {
+        MAN("m"),
+        WOMAN("f"),
+        OTHER("bot")
+    }
 
     private val _indicatorState: MutableLiveData<IndicatorState> = MutableLiveData(IndicatorState.NORMAL)
     val indicatorState: LiveData<IndicatorState> = _indicatorState
 
-
+    val email: MutableLiveData<String> = MutableLiveData("")
+    val name: MutableLiveData<String> = MutableLiveData("")
+    val password: MutableLiveData<String> = MutableLiveData("")
+    val gender: MutableLiveData<String> = MutableLiveData(Gender.MAN.realValue)
+    val birthday: MutableLiveData<String> = MutableLiveData("")
+    val question: MutableLiveData<String> = MutableLiveData("")
+    val answer: MutableLiveData<String> = MutableLiveData("")
 }
