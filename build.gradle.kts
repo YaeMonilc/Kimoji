e15f75ecc3c9b46fa799ae75cded16102e9ffcd0
YaeMonilc <Monilc@outlook.com>
2024-07-23 20:42:19 +0800

    增加 Safe args

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,12 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.navigation.safeargs) apply false
+}
+
+buildscript {
+    dependencies {
+        // https://mvnrepository.com/artifact/androidx.navigation.safeargs/androidx.navigation.safeargs.gradle.plugin
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+    }
 }
