ee6f76e40c6f8390424e142703d7d558bba2a17e
YaeMonilc <Monilc@outlook.com>
2024-07-24 22:17:28 +0800

    增加更多本地化支持

--- app/src/main/java/moe/wisteria/android/kimoji/util/LocalizationUtils.kt
+++ app/src/main/java/moe/wisteria/android/kimoji/util/LocalizationUtils.kt
@@ -3,7 +3,9 @@ package moe.wisteria.android.kimoji.util
 import moe.wisteria.android.kimoji.R
 
 val localizationMapping: Map<String, Int> = mapOf(
-    "invalid email or password" to R.string.network_invalid_email_or_password
+    "validation error" to R.string.network_validation_error,
+    "invalid email or password" to R.string.network_invalid_email_or_password,
+    "email is already exist" to R.string.network_email_is_already_exist
 )
 
 fun getLocalization(
