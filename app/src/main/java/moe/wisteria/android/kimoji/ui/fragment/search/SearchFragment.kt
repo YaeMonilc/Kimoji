ac634414e60212a7363bdbebcb4b19b635ba6e67
YaeMonilc <Monilc@outlook.com>
2024-07-25 18:59:28 +0800

    移除未使用的引入

--- app/src/main/java/moe/wisteria/android/kimoji/ui/fragment/search/SearchFragment.kt
+++ app/src/main/java/moe/wisteria/android/kimoji/ui/fragment/search/SearchFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.app.ActionBar
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import moe.wisteria.android.kimoji.databinding.FragmentSearchBinding
