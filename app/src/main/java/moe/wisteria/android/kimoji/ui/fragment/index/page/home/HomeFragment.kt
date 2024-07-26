6d9f847c900754ba8d25f5160553e132578303d6
YaeMonilc <Monilc@outlook.com>
2024-07-26 17:56:11 +0800

    修正列表滚动不能复原

--- app/src/main/java/moe/wisteria/android/kimoji/ui/fragment/index/page/home/HomeFragment.kt
+++ app/src/main/java/moe/wisteria/android/kimoji/ui/fragment/index/page/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import androidx.datastore.preferences.core.edit
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import moe.wisteria.android.kimoji.R
 import moe.wisteria.android.kimoji.databinding.FragmentHomeBinding
 import moe.wisteria.android.kimoji.network.entity.response.PicaResponse.Companion.onError
@@ -62,7 +63,9 @@ class HomeFragment : BaseFragment(
                         showSnackBar(it.author, binding.fragmentHomeComicList)
                     },
                     comicList = listOf()
-                )
+                ).apply {
+                    stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+                }
                 layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
             }
 
