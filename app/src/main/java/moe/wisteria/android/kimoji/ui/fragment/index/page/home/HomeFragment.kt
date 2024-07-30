667296c6e081a04053638c9cda95c5c6fa3bba18
YaeMonilc <Monilc@outlook.com>
2024-07-30 21:13:54 +0800

    移除未使用的引入

--- app/src/main/java/moe/wisteria/android/kimoji/ui/fragment/index/page/home/HomeFragment.kt
+++ app/src/main/java/moe/wisteria/android/kimoji/ui/fragment/index/page/home/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.datastore.preferences.core.edit
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import moe.wisteria.android.kimoji.R
