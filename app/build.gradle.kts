afac6fce5be9020b13c13f5cb82122974cbae1de
YaeMonilc <Monilc@outlook.com>
2024-07-31 18:05:11 +0800

    修改版本为 1.0.1-八奈見杏菜

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "moe.wisteria.android.kimoji"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1-八奈見杏菜"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
