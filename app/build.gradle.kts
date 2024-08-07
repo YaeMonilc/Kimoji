719df46ddf7921e799189998a39a272a7b169df9
YaeMonilc <Monilc@outlook.com>
2024-08-07 11:07:45 +0800

    更新版本 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "moe.wisteria.android.kimoji"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1-八奈見杏菜"
+        versionCode = 3
+        versionName = "1.0.2-八奈見杏菜"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
